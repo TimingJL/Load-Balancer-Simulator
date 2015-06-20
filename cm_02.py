2bc375f8e52c1cf6ebc7f4b3f07060890fb51f04
Timing <eefozeo@gmaiml.com>
2015-06-20 08:47:16 -0400

    to simulate queue and service time of Opnefire connection manager

--- cm_02.py
+++ cm_02.py
@@ -7,6 +7,9 @@ import numpy as np
 from collections import deque
 from threading import Thread
 import parameter
+import redis
+
+
 
 credentials = pika.PlainCredentials(parameter.rabbitmq_username, parameter.rabbitmq_password)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=parameter.rabbitmq_host,port = 5672, virtual_host = parameter.rabbitmq_vhost, credentials = credentials))
@@ -15,30 +18,36 @@ channel = connection.channel()
 queue_name = 'cm_02'
 msg_queue = deque([])
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r.set(queue_name, 0)
+
 channel.queue_declare(queue=queue_name, durable=True)
 print ' [*] Waiting for messages. To exit press CTRL+C'
 
 
 def out():
     while True:        
-        if msg_queue:
-            s = float(msg_queue.popleft())
-            time.sleep(s)
-
-            print '\n pop :' + str(s)
+        if msg_queue:          
+            s = float(msg_queue.popleft()[0])
+            print ' pop'
             print 'msg_queue: %r' %(msg_queue)
+            r.set(queue_name, len(msg_queue))  
+            print 'queue length: %r' %(r.get(queue_name))
             print '\n\n'
             
+            time.sleep(s)
 
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
     msg_queue.append(body)
-    print ' in queue'
-    print msg_queue
-    print '\n\n'
     ch.basic_ack(delivery_tag = method.delivery_tag)
-    #thread.start_new_thread(Threadfun, (msg_queue, 2, lock))
+    r.set(queue_name, len(msg_queue))
+
+    print ' in queue'
+    print 'msg_queue: %r' %(msg_queue)
+    print 'queue length: %r' %(r.get(queue_name))
+    print '\n\n'
+
 
 th = Thread(target=out)
 th.start()
